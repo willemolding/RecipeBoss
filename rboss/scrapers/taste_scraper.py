341d3fd82c6c76e08c21883b78274f2c65eee981
willemolding <willemolding@gmail.com>
2015-07-04 16:24:14 +0800

    adds taste recipe scaper

---
+++ rboss/scrapers/taste_scraper.py
@@ -0,0 +1,34 @@
+"""
+Parses recipes from taste.com to produce lists of ingredients and steps
+"""
+from bs4 import BeautifulSoup
+import requests
+
+class TasteRecipeScraper:
+
+	def __init__(self, url):
+		response = requests.get(url)
+		self.soup = BeautifulSoup(response.text)
+
+	def get_ingredients(self):
+		# first locate all the ingredients.
+		ingredients = []
+		for table in self.soup.find_all("ul", class_="ingredient-table"):
+			for ingredient in table.find_all("li"):
+				ingredients.append(ingredient.get_text(strip=True))
+		return ingredients
+
+	def get_steps(self):
+		# locate each of the steps of the recipe
+		steps = []
+		for step in self.soup.find_all("p", itemprop="recipeInstructions"):
+			steps.append(step.get_text(strip=True))
+		return steps
+
+	def get_steps_string(self):
+		steps = self.get_steps()
+		string = ''
+		for step in steps:
+			string += step
+		return string
+
